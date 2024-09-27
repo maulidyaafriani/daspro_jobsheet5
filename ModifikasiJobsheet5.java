8c677283e780df3131a7908e32dfe0d81d6aa4dc
maulidyaafriani <maulidyafriani@gmail.com>
2024-09-27 10:40:22 +0700

    Add files via upload
    menambahkan modifikasijobsheet5

---
+++ ModifikasiJobsheet5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ModifikasiJobsheet5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        String hasil = (angka % 2 == 0) ? " Termasuk bilangan genap" : "Termasuk bilangan ganjil";
+        
+        System.out.println("Angka " + angka + hasil);
+        
+        sc.close();
+
+
+        
+        
+
+
+
+    }
+    
+}
